d8c714c94eff5115a692a94a263ce84de2e8a0d7
Paul Rodwell <paul.rodwell@btinternet.com>
2014-06-02 12:30:35 +0100

    correct index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
+#!/usr/bin/env node
 // **index.js**
-// Simple file so that if you require this directory
-// in node it instead requires ./cli.coffee
-// with coffee-script already loaded.
 require('coffee-script');
 module.exports = require('./cli');
+
