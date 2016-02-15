c6202137dadd9ebc6ee35f27fd8af9d3a04e4c53
Ward Cunningham <ward@c2.com>
2016-02-15 11:41:43 -0800

    add splash screen while server starts.

--- electron-wiki.js
+++ electron-wiki.js
@@ -1,10 +1,22 @@
 var app = require('app')
 var BrowserWindow = require('browser-window')
-var server = require('child_process').fork('./index.js')
+var server = require('child_process').fork('./index.js', ['-p', '3333'])
 
 app.on('ready', function () {
-  var browser = new BrowserWindow({ width: 1024, height: 768 })
-  browser.loadURL('file://' + __dirname + '/electron-frame.html')
+  var browser = new BrowserWindow({ width: 1024, height: 768})
+  browser.loadURL('file://' + __dirname + '/electron-splash.html')
+  setTimeout(start, 5000)
+  setTimeout(reload, 7000)
+
+  function start () {
+    console.log('start')
+    browser.loadURL('file://' + __dirname + '/electron-frame.html')
+  }
+
+  function reload () {
+    console.log('reload')
+    browser.reload()
+  }
 })
 
 app.on('will-quit', function () {
